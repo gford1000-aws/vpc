8909ee6b7d4b94cf366b31ece4d46780ff4678b7
Geoff Ford <gford1000-aws@outlook.com>
2017-07-13 20:16:58 +1000

    Example of use of the module

---
+++ terraform/example/vpc.tf
@@ -0,0 +1,21 @@
+module "vpc" {
+        source = "github.com/gford1000-aws/vpc//terraform/module"
+
+        cidr_address = "10.0"
+        private_subnet_count = 1
+        create_public_subnet = true
+        vpc_name = "MyNewVPC"
+        multiple_nat_gateways = false
+}
+
+output "vpc_id" {
+        value = "${module.vpc.vpc}"
+}
+
+output "public_subnets" {
+        value = "${module.vpc.public_subnets}"
+}
+
+output "private_subnets" {
+        value = "${module.vpc.private_subnets}"
+}
